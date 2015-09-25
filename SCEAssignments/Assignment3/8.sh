d9d0d6e92fa103ed0915df310b29f9129a89308a
ChokshiUtsav <uvc.smart.29@gmail.com>
2015-09-26 05:01:02 +0530

    added display opeation in 8.sh

--- SCEAssignments/Assignment3/8.sh
+++ SCEAssignments/Assignment3/8.sh
@@ -21,7 +21,7 @@ dbFile="$dbFile"".db"
 `touch $dbFile`
 #echo $dbFile
 
-#
+#asking for options
 echo "1.insert(i)"
 echo "2.remove(r)"
 echo "3.display(d)"
@@ -29,6 +29,7 @@ echo "4.select(s)"
 read -p "Please select one of the above option : " option
 
 
+#Processing insert operation
 temp=""
 flag=0
 if [ $option = "i" ]
@@ -48,6 +49,7 @@ then
 	echo $temp >> $dbFile
 fi
 
+#Processing remove operation
 if [ $option = "r" ]
 then
 	totalLines=`wc -l $dbFile | awk '{ print $1 }'`	
@@ -61,10 +63,47 @@ then
     fi
 fi
 
+flag=0
+str="-----------------------------------------------------------------"
+#Processing display operation
+if [ $option = "d" ]
+then
+	for i in ${fieldNames[@]}
+	do
+		if [ $flag -eq 0 ]
+		then
+			printf "%-15s " "$i"
+			flag=1
+		else
+			printf "| %-10s " "$i"
+	    fi				
+	done
+	printf "\n"
+	printf "%s\n" "$str"
 
-#if [ $option -eq d ]
-#then
-#fi
+	IFS=','	
+	readarray -t line <<< "`cat $dbFile`"
+	flag=0
+	count=1
+	for i in ${line[@]}
+	do
+		if [ $flag -eq 0 ]
+		then
+			printf "%-15s " "$i"
+			flag=1
+		else
+			printf "| %-10s " "$i"
+	    fi
+	    
+	    count=`expr $count + 1 `
+	    if [ $count -gt $totalFields ]
+	    then
+	    	printf "\n"
+	    	flag=0
+	    	count=1
+	    fi
+	done
+fi
 
 #if [ $option -eq s ]
 #then
